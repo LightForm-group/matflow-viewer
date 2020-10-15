dc7bb98cdb137ed1560d182dde3a970b96611950
aplowman <adam.jp@live.co.uk>
2020-10-15 14:51:14 +0100

    Add to requirements packages for virtualenv

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='matflow-viewer',
     version='0.1.0',
     install_requires=[
-        # 'matflow',
+        'matflow',
         'jinja2_highlight',
         'pygithub',
         'flask',
